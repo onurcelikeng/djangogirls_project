7b916c12de69de9988d94594e9906a9397d44343
Onur Celik <onrcelik@outlook.com>
2017-03-28 23:08:54 +0300

    django girls project, first commit

---
+++ blog/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    crate_date = models.DateTimeField(default=timezone.now)
+    publish_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.publish_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
